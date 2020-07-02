b48ec8dc9f873101d34a05260618e95fa6e8f330
Lee jun yong <junyoung1818@gmail.com>
2020-07-02 15:15:49 +0900

    걷기 애니메이션

---
+++ Walking_robot_animaion.c
@@ -0,0 +1,263 @@
+﻿/*
+ * Copyright (c) 1993-2003, Silicon Graphics, Inc.
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose and without fee is hereby granted, provided that the above
+ * copyright notice appear in all copies and that both the copyright
+ * notice and this permission notice appear in supporting documentation,
+ * and that the name of Silicon Graphics, Inc. not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.
+ *
+ * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS" AND
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
+ * GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND,
+ * OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION, LOSS OF
+ * PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF THIRD
+ * PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN ADVISED OF
+ * THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE POSSESSION, USE
+ * OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * US Government Users Restricted Rights
+ * Use, duplication, or disclosure by the Government is subject to
+ * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
+ * (c)(1)(ii) of the Rights in Technical Data and Computer Software
+ * clause at DFARS 252.227-7013 and/or in similar or successor clauses
+ * in the FAR or the DOD or NASA FAR Supplement.  Unpublished - rights
+ * reserved under the copyright laws of the United States.
+ *
+ * Contractor/manufacturer is:
+ *	Silicon Graphics, Inc.
+ *	1500 Crittenden Lane
+ *	Mountain View, CA  94043
+ *	United State of America
+ *
+ * OpenGL(R) is a registered trademark of Silicon Graphics, Inc.
+ */
+
+ /*
+  * robot.c
+  * This program shows how to composite modeling transformations
+  * to draw translated and rotated hierarchical models.
+  * Interaction:  pressing the s and e keys (shoulder and elbow)
+  * alters the rotation of the robot arm.
+  */
+
+#include <GL/glut.h>
+#include <stdlib.h>
+
+
+static int leg1 = 0, knee1 = 0;
+static int leg2 = 0, knee2 = 0;
+static float root = -3.0;
+static int seq = 0;
+
+static GLfloat xRot = 0.0;
+static GLfloat yRot = 0.0;
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+	glTranslatef(root, 0.0, 0.0);
+	root += 0.1;
+
+	glRotatef(-90.0, 0.0, 0.0, 1.0);
+
+	// left leg
+
+	glPushMatrix();
+
+	glTranslatef(0.0, 0.0, -0.1);
+	glPushMatrix();
+	glTranslatef(-1.0, 0.0, 0.0);
+	glRotatef((GLfloat)leg1, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef((GLfloat)knee1, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPopMatrix();
+
+	//right leg
+
+	glPushMatrix();
+
+	glTranslatef(0.0, 0.0, 0.1);
+
+	glPushMatrix();
+	glTranslatef(-1.0, 0.0, 0.0);
+	glRotatef((GLfloat)leg2, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef((GLfloat)knee2, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -5.0);
+}
+
+void TimerFunction(int value)
+{
+	if ((seq >= 0) && (seq < 10))
+	{
+		leg1 = (leg1 + 4) % 360;
+		leg2 = (leg2 - 4) % 360;
+
+		knee1 = (knee1 - 2) % 360;
+		knee2 = (knee2 - 2) % 360;
+
+		seq++;
+		glutPostRedisplay();
+	}
+	else if ((seq >= 10) && (seq < 20))
+	{
+		leg1 = (leg1 - 4) % 360;
+		leg2 = (leg2 + 4) % 360;
+
+		knee1 = (knee1 + 2) % 360;
+		knee2 = (knee2 + 2) % 360;
+
+		seq++;
+		glutPostRedisplay();
+	}
+	else if ((seq >= 20) && (seq < 30))
+	{
+		leg1 = (leg1 - 4) % 360;
+		leg2 = (leg2 + 4) % 360;
+
+		knee1 = (knee1 - 2) % 360;
+		knee2 = (knee2 - 2) % 360;
+
+		seq++;
+		glutPostRedisplay();
+	}
+	else if ((seq >= 30) && (seq < 40))
+	{
+		leg1 = (leg1 + 4) % 360;
+		leg2 = (leg2 - 4) % 360;
+
+		knee1 = (knee1 + 2) % 360;
+		knee2 = (knee2 + 2) % 360;
+
+		seq++;
+		glutPostRedisplay();
+	}
+	if (seq >= 40)
+	{
+		seq = 0;
+		if (root > 3.0)
+			root = -3.0;
+	}
+	glutPostRedisplay();
+	glutTimerFunc(33, TimerFunction, 1);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 's':
+		leg1 = (leg1 + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'S':
+		leg1 = (leg1 - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'e':
+		knee1 = (knee1 + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'E':
+		knee1 = (knee1 - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'l':
+		leg2 = (leg2 + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'L':
+		leg2 = (leg2 - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'k':
+		knee2 = (knee2 + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'K':
+		knee2 = (knee2 - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutTimerFunc(330, TimerFunction, 1);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
+}
